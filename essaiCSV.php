99ea047d52a709acc6e5657d9633dff61fab7c4d
coutcout <valcou@aliceadsl.fr>
2014-04-24 15:56:57 +0200

    Suppression fichier inutile

--- essaiCSV.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once("/outils_php/autoload.php");
-
-$csv = new CSVLoader("excel/etapes.csv",";");
-
-print_r($csv->getHeadLine());
-
-print_r($csv->getIndexTitle(array("etape", "code")));
